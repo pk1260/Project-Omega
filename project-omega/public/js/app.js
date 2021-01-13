0a342a53af4e04d19c810595544ccdfc4e37b2a2
pk1260 <43113381+pk1260@users.noreply.github.com>
2021-01-13 23:58:31 +0100

    Main commits for auth

--- project-omega/public/js/app.js
+++ project-omega/public/js/app.js
@@ -19384,8 +19384,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\xampp\htdocs\Project-Omega\project-omega\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\xampp\htdocs\Project-Omega\project-omega\resources\scss\styles.scss */"./resources/scss/styles.scss");
+__webpack_require__(/*! D:\XAMPP\htdocs\Project-Omega\project-omega\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\XAMPP\htdocs\Project-Omega\project-omega\resources\scss\styles.scss */"./resources/scss/styles.scss");
 
 
 /***/ })
---
+++ project-omega/resources/views/layouts/master.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Project Omega</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Fav icons -->
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/Project-Omega-Logo-Fav-16x16.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/Project-Omega-Logo-Fav-16x16.png') }}">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <div class="content-container">
+        <h1>hello</h1>
+    </div>
+</body>
+</html>
